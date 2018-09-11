7c3051402baa4aa94078cf1661b4eb815234c9a2
JLF <jean-luc.falcone@unige.ch>
2018-09-11 23:07:00 +0200

    Nonzeros and pretty print via show method

--- src/main/scala/Clause.scala
+++ src/main/scala/Clause.scala
@@ -1,6 +1,16 @@
 package ch.hepia.scalinea
 
-sealed trait Sign 
+sealed trait Sign {
+  import Sign._
+  def show: String = this match {
+    case Eq => "="
+    case NonEq => "≠"
+    case BigEq => "≥"
+    case LessEq => "≤"
+    case Big => ">"
+    case Less => "<"
+  }
+}
 object Sign {
   case object Eq extends Sign
   case object NonEq extends Sign
@@ -15,9 +25,12 @@ object MathUtil {
   def nonZero(value: Double): Boolean = math.abs(value) > delta
 }
 
-case class Constant(value: Double)
-
-case class NonZeroConstant private(value: Double)
+case class Constant(value: Double) {
+  def show: String = value.toString
+}
+case class NonZeroConstant private(value: Double) {
+  def show: String = value.toString
+}
 object NonZeroConstant {
   def apply(value: Double): Option[NonZeroConstant] = 
     if(MathUtil.nonZero(value)) 
@@ -26,22 +39,59 @@ object NonZeroConstant {
       None
 }
 
-case class Var(symbol: String)
+case class Exponent private ( value: Int ) {
+  def show: String = value.toString
+}
+object Exponent {
+  def apply( value: Int ): Option[Exponent] = 
+    if( value != 0 )
+      Some( new Exponent(value) )
+    else
+      None
+}
 
-case class Vars(value: Map[Var, Int])
+case class Var(symbol: String) {
+  def show: String = symbol
+}
 
-case class Terms(terms: Map[Vars, Constant])
+case class Vars(value: Map[Var, Exponent]) {
+  def show: String = {
+    value.toList.sortBy( _._1.symbol ).map{
+      case (v,ex) if ex.value == 1 => v.show
+      case (v,ex) => v.show+"^"+ex.show
+    }.mkString("*")
+  }
+}
 
-case class Clause(terms: Terms, sign: Sign, constant: Constant)
+case class Terms(terms: Map[Vars, NonZeroConstant]) {
+  def show: String = {
+    terms.toList.map{
+      case (vs,cst) => (cst.show,vs.show)
+    }.sortBy( _._2).map{
+      case (cst,vs) => cst+"*"+vs
+    }.mkString(" + ")
+  }
+}
 
+case class Clause(terms: Terms, sign: Sign, constant: Constant) {
+  def show: String =
+     terms.show + " " + sign.show + " " + constant.show
+}
 
 object Demo extends App {
   val x = Var("x")
   val y = Var("y")
-  val terms = Terms( Map( Vars(Map( x->2, y->1 ))->Constant(2), Vars(Map(x->1))->Constant(5)) )
+  val one = Exponent(1).get
+  val two = Exponent(2).get
+  val twoC = NonZeroConstant(2).get
+  val fiveC = NonZeroConstant(5).get
+  val terms =
+    Terms(
+      Map( Vars(Map( x->two, y->one ))->twoC, Vars(Map(x->one))->fiveC) )
   val clause = Clause( terms, Sign.Less, Constant(5) )
 
   println( clause )
+  println( clause.show )
 
 }
 
