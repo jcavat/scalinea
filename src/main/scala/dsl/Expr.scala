dbce0100d445a458280430bef4fb6be1405924b9
Joel Cavat <jcavat@gmail.com>
2018-09-14 15:21:10 +0200

    Add all operations on constraints

--- src/main/scala/dsl/Expr.scala
+++ src/main/scala/dsl/Expr.scala
@@ -22,10 +22,18 @@ sealed trait Constr {
 
   def toClause: clause.Clause = this match {
     case LessEq(lhs, rhs) => clause.Clause( (lhs-rhs).toTerms , clause.Sign.LessEq )
+    case Less(lhs, rhs) => clause.Clause( (lhs-rhs).toTerms , clause.Sign.Less )
+    case BigEq(lhs, rhs) => clause.Clause( (lhs-rhs).toTerms , clause.Sign.BigEq )
+    case Big(lhs, rhs) => clause.Clause( (lhs-rhs).toTerms , clause.Sign.Big )
+    case Eq(lhs, rhs) => clause.Clause( (lhs-rhs).toTerms , clause.Sign.Eq )
   }
 }
 
 case class LessEq(lhs: Expr, rhs: Expr) extends Constr
+case class Less(lhs: Expr, rhs: Expr) extends Constr
+case class BigEq(lhs: Expr, rhs: Expr) extends Constr
+case class Big(lhs: Expr, rhs: Expr) extends Constr
+case class Eq(lhs: Expr, rhs: Expr) extends Constr
 
 
 
@@ -38,19 +46,34 @@ object Ops {
     def *( rhs: Double ) = Mult( lhs, Const(rhs) )
     def -( rhs: Expr ) = Add( lhs, Const(-1)*rhs )
     def -( rhs: Double ) = Add( lhs, Const(-1)*rhs )
-    def <=( rhs: Expr ) = LessEq(lhs, rhs)
+    def <=( rhs: Expr ) = LessEq( lhs, rhs )
+    def <=( rhs: Double ) = LessEq( lhs, Const(rhs) )
+    def <( rhs: Expr ) = Less( lhs, rhs )
+    def <( rhs: Double) = Less( lhs, Const(rhs) )
+    def >=( rhs: Expr ) = BigEq( lhs, rhs )
+    def >=( rhs: Double ) = BigEq( lhs, Const(rhs) )
+    def >( rhs: Expr ) = Big( lhs, rhs )
+    def >( rhs: Double ) = Big( lhs, Const(rhs) )
+    def ===( rhs: Expr ) = Eq( lhs, rhs )
+    def ===( rhs: Double) = Eq( lhs, Const(rhs) )
   }
 
-  implicit class RichDouble( lhs: Double ) {
+  implicit class RichDouble( val lhs: Double ) extends RichLiteral
+
+  implicit class RichInt( val lhsArg: Int ) extends RichLiteral {
+    val lhs: Double = lhsArg.toDouble
+  }
+
+  sealed trait RichLiteral {
+    def lhs: Double
     def +( rhs: Expr ) = Add( Const(lhs), rhs )
     def *( rhs: Expr ) = Mult( Const(lhs), rhs )
     def -( rhs: Expr ) = Add( Const(lhs), Const(-1)*rhs )
-  }
-
-  implicit class RichInt( lhs: Int ) {
-    def +( rhs: Expr ) = Add( Const(lhs.toDouble), rhs )
-    def *( rhs: Expr ) = Mult( Const(lhs.toDouble), rhs )
-    def -( rhs: Expr ) = Add( Const(lhs.toDouble), Const(-1)*rhs )
+    def <=( rhs: Expr ) = LessEq( Const(lhs), rhs )
+    def <( rhs: Expr ) = Less( Const(lhs), rhs )
+    def >=( rhs: Expr ) = BigEq( Const(lhs), rhs )
+    def >( rhs: Expr ) = Big( Const(lhs), rhs )
+    def ===( rhs: Expr ) = Eq( Const(lhs), rhs )
   }
 
 }
@@ -64,7 +87,7 @@ object Demo  extends App {
 
   import Ops._
 
-  val e = 2+x <= 4*y
+  val e = 2*x*x <= 4*x
 
   println(e)
   
