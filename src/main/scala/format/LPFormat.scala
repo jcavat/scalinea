d3fd9867ebde21055ef81a0f954a0e49f98fefb0
Joel Cavat <jcavat@gmail.com>
2018-10-20 18:11:21 +0200

    Move LpFormat and use TypeClasses for lp format

--- src/main/scala/format/LPFormat.scala
+++ src/main/scala/format/LPFormat.scala
@@ -3,42 +3,51 @@ package format
 
 import ch.hepia.scalinea.clause.Sign._
 import ch.hepia.scalinea.clause.{Clause, Sign, Terms, Vars}
-import ch.hepia.scalinea.util.LpFormat
+
 
 
 object LPFormat extends Format[Iterable[String]] {
 
-  implicit val canExportSignToLp = LpFormat.instance[Sign]{
-    case Eq => "="
-    case NonEq => "!="
-    case BigEq => ">="
-    case LessEq => "<="
-    case Big => ">"
-    case Less => "<"
+  trait LpFormattable {
+    def toLp: String
   }
 
-  implicit val canExportVarsToLp = LpFormat.instance[Vars]{ vs =>
-    vs.sortedVars.map{ v =>
-      val exponent = vs.value(v).value
-      if( exponent == 1 )
-        v.symbol
-      else
-        v.symbol+" ^ "+exponent.toString
-    }.mkString(" ")
+  implicit class SignLpFormattable(s: Sign) extends LpFormattable {
+    override def toLp: String = s match {
+      case Eq => "="
+      case NonEq => "!="
+      case BigEq => ">="
+      case LessEq => "<="
+      case Big => ">"
+      case Less => "<"
+    }
   }
 
-  implicit val canExportToTermsLp = LpFormat.instance[Terms]{ ts =>
+  implicit class VarsLpFormattable(vs: Vars) extends LpFormattable {
+    override def toLp: String = {
+      vs.sortedVars.map {
+        v =>
+          val exponent = vs.value (v).value
+          if (exponent == 1)
+            v.symbol
+          else
+            v.symbol + " ^ " + exponent.toString
+      }.mkString (" ")
+    }
+  }
 
-    val (linearVars, quadVars) = ts.sortedVars.partition( vars => vars.isLinear )
+  implicit class TermsLpFormattable(ts: Terms) extends LpFormattable {
+    override def toLp: String = {
+      val (linearVars, quadVars) = ts.sortedVars.partition(vars => vars.isLinear)
 
-    val toStr: List[Vars] => List[String] =
-      _.map( vars => ts.terms(vars).value.toString + " " + LpFormat[Vars].asString(vars) )
+      val toStr: List[Vars] => List[String] =
+        _.map(vars => ts.terms(vars).value.toString + " " + vars.toLp)
 
-    val linearVarsExpr = toStr(linearVars).mkString(" + ")
-    val quadVarsExpr = if (quadVars.isEmpty) "" else " [ " + toStr(quadVars).mkString(" + ") + " ] "
+      val linearVarsExpr = toStr(linearVars).mkString(" + ")
+      val quadVarsExpr = if (quadVars.isEmpty) "" else " [ " + toStr(quadVars).mkString(" + ") + " ] "
 
-    linearVarsExpr + quadVarsExpr
-
+      linearVarsExpr + quadVarsExpr
+    }
   }
 
   def apply( system: clause.System ):  Output[Iterable[String]] = {
@@ -50,18 +59,17 @@ object LPFormat extends Format[Iterable[String]] {
         // If constant exists in terms, put it on the right side of que constraint equation
         ts.terms.get( Vars(Map()) ) match {
           case Some( v ) =>
-            LpFormat[Terms].asString(Terms(ts.terms - Vars(Map()))) + " " +
-              LpFormat[Sign].asString(sign) + " " + -v.value
+            Terms(ts.terms - Vars(Map())).toLp + " " +
+              sign + " " + -v.value
 
           case None =>
-            LpFormat[Terms].asString(ts) + " " +
-              LpFormat[Sign].asString(sign) + " 0"
+            ts.toLp + " " +
+              sign.toLp + " 0"
         }
       }
     }
 
     Success(lps, Nil)
-
 
   }
 
