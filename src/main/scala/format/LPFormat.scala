470a5411ecbc127b13534559fa552d4d8ec2d848
Joel Cavat <jcavat@gmail.com>
2018-10-20 11:19:59 +0200

    Move clause to lp format logic

--- src/main/scala/format/LPFormat.scala
+++ src/main/scala/format/LPFormat.scala
@@ -1,12 +1,35 @@
 package ch.hepia.scalinea
 package format
 
+import ch.hepia.scalinea.clause.{Clause, Sign, Terms, Vars}
+import ch.hepia.scalinea.util.LpFormat
+
 
 object LPFormat extends Format[Iterable[String]] {
 
+  def apply( system: clause.System ):  Output[Iterable[String]] = {
+    val clauses = system.constraints
+
+    val lps: List[String] = clauses.map {
+      case Clause(ts,sign) => {
+
+        // If constant exists in terms, put it on the right side of que constraint equation
+        ts.terms.get( Vars(Map()) ) match {
+          case Some( v ) =>
+            LpFormat[Terms].asString(Terms(ts.terms - Vars(Map()))) + " " +
+              LpFormat[Sign].asString(sign) + " " + -v.value
+
+          case None =>
+            LpFormat[Terms].asString(ts) + " " +
+              LpFormat[Sign].asString(sign) + " 0"
+        }
+      }
+    }
+
+    Success(lps, Nil)
 
 
-  def apply( system: clause.System ):  Output[Iterable[String]] = ???
+  }
 
 }
 
