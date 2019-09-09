2c63f9e72496f9f63ff9c106cfbcd14355c81ce3
Joel Cavat <jcavat@gmail.com>
2019-09-09 15:46:35 +0200

    Write lp feature in a Helper

--- src/main/scala/ch/hepia/scalinea/solver/Solver.scala
+++ src/main/scala/ch/hepia/scalinea/solver/Solver.scala
@@ -7,21 +7,29 @@ trait Solver {
   def solve( system: clause.System ): Unit
 }
 
+object SolverUtil {
+  def writeLpFile(lines: Iterable[String], filename: String) = {
+    import java.io.File
+    import java.io.PrintWriter
+
+    val writer = new PrintWriter(new File(filename))
+    lines.foreach(writer.write)
+    writer.close()
+  }
+}
+
 
 object FakeLpSolver extends Solver {
-  import java.io.File
-  import java.io.PrintWriter
 
-  override def solve(system: clause.System): Unit = {
-
-    val writer = new PrintWriter(new File("test.lp"))
+  def solve(system: clause.System): Unit = {
 
     system.exportTo(LPFormat) match {
-      case Success(result,_) => result.map(_ + "\n").foreach( writer.write )
-      case Failure(_,_) => println("Oups")
+      case Success(result, _) => {
+        SolverUtil.writeLpFile(result.map(_ + "\n"), "test.lp")
+      }
+      case Failure(_, _) => println("Oups")
     }
 
-    writer.close()
-
   }
+
 }
