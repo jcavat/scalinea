536f1e1e7f6fe6bae7f334a48139e8282354de14
Joel Cavat <jcavat@gmail.com>
2019-09-12 10:06:51 +0200

    Use a map of vars

--- src/main/scala/ch/hepia/scalinea/dsl/System.scala
+++ src/main/scala/ch/hepia/scalinea/dsl/System.scala
@@ -77,31 +77,31 @@ object SysDemo extends App {
   val profs = List("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11", "p12")
   val days = List("mo", "tu", "we", "th", "fr")
   val pref = Map(
-    "p1" -> Seq(("mo", 3), ("tu", 2)).toMap
+    "p1" -> Map("mo" -> 3, "tu" -> 2),
+    "p12" -> Map("th" -> 4, "fr" -> 1)
   )
 
-  val vars: Seq[BVar] = for {
+  val vars = (for {
     p <- profs
     d <- days
-  } yield BVar(s"${p}_$d")
-  val mapVars = vars.map( v => v.symbol -> v ).toMap
+  } yield BVar(s"${p}_$d")).map( v => v.symbol -> v ).toMap
 
   val system: clause.System = {
     dsl.System.define.constraints(
       // it should have at least two professors per day
-      for(d <- days) yield sum(vars.filter( _.symbol.endsWith(s"_$d") ) ) >= 2
+      for(d <- days) yield sum(vars.values.filter( _.symbol.endsWith(s"_$d") ) ) >= 2
     ).constraints(
       // a professor should work only one day
-      for(p <- profs) yield sum(vars.filter( _.symbol.startsWith(s"${p}_") ) ) === 1
+      for(p <- profs) yield sum(vars.values.filter( _.symbol.startsWith(s"${p}_") ) ) === 1
     ).constraints(
       // if p1 works on monday, p2 works on monday, too
-      mapVars("p1_mo") <= mapVars("p2_mo")
+      vars("p1_mo") <= vars("p2_mo")
     ).maximize(
       sum(for {
         p <- profs
         d <- days
         if pref.get(p).flatMap(prefProf => prefProf.get(d) ).isDefined
-      } yield pref(p)(d) * mapVars(s"${p}_$d")
+      } yield pref(p)(d) * vars(s"${p}_$d")
       )
     ).build
   }
@@ -110,8 +110,8 @@ object SysDemo extends App {
   val res: Output[LPResult] = solver.solve(system)
   res match {
     case Success(sol: Solution, _) => {
-      println("*" * 10 + "\n" + sol.isOptimal )
-      for( v <- vars ) {
+      println("*" * 10 + "\nOptimal: " + sol.isOptimal )
+      for( v <- vars.values ) {
         if (sol(v))
           println( s"${v.symbol}: ${sol(v)}" )
       }
